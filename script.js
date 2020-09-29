7bce6666fbb86ad9fce9f2bdc9dffeac48e02268
armoako <faboafo@outlook.com>
2020-09-29 17:51:33 +0000

    fixed minor issues

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let colorOptionTwo = document.getElementById("pick-color2")
 let body = document.getElementById("main")
 
 const backGroundColor = () => {
-	// body...
 	body.style.background = "linear-gradient(to right, " + colorOptionOne.value + ", " + colorOptionTwo.value + ")"
 	h3.textContent = body.style.background + ";"
 }
